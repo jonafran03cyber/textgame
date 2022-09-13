e8fc3b92efd962d96efb83703054e138a40e7c49
defensepanda <defensepanda@proton.me>
2022-09-13 18:04:04 +0200

    Add hourly choice

--- main.py
+++ main.py
@@ -1,19 +1,66 @@
 from os import system
-
+import time
+alive = True
 
 u_name = input("What is your name?\n")
 valid_choise_byhour = {
-    "6":["Shine shoes"],
-    "7":["Shine shoes"],
-    "8":["Shine shoes"],
-    "9":["Shine shoes"]
+    "6":["shine shoes"],
+    "7":["shine shoes"],
+    "8":["shine shoes"],
+    "9":["shine shoes"],
+    "10":["shine shoes"],
+    "11":["shine shoes"],
+    "12":["shine shoes"],
+    "13":["shine shoes"],
+    "14":["shine shoes"],
+    "15":["shine shoes"],
+    "16":["shine shoes"],
+    "17":["shine shoes"],
+    "18":["shine shoes"],
+    "19":["shine shoes"],
+    "20":["shine shoes"],
+    "21":["shine shoes"],
+    "22":["shine shoes"],
+    "23":["shine shoes"]
+}
+core_stats = {
+    "shoe_shine":0,
+    "energy": 100
 }
 
+def choice_string_by_hour(hour, hourly_list):
+    options = hourly_list[str(hour)]
+    temp_str = "You can "
+    for index, choice in enumerate(options):      
+        temp_str = temp_str + choice
+        if index + 1 < len(options):
+            temp_str = temp_str + " OR "
+    return temp_str + "!"
 
-while True:
-    for i in range(6,23,1):
+
+
+while alive:
+    for h in range(6,23,1):
         choice = ""
         system("cls")
-        print(f"The clock is now {i}:00")
-        while choice not in valid_choise_byhour[str(i)]:
-            choice = input("What to do\n")
+        print(f"The clock is now {h}:00, {u_name}, what to do?")
+        # You can do ____ OR ____
+        print(choice_string_by_hour(h,valid_choise_byhour))
+
+        #Choose task
+        tmp_cnt = 0
+        while choice not in valid_choise_byhour[str(h)]:
+            if tmp_cnt == 0:
+                choice = input("What do you do now?\n").lower()
+            else:
+                choice = input("Not valid option\n").lower()
+            tmp_cnt +=1
+
+        system("cls")
+        print(f"Great, you proceed to {choice.capitalize()}") 
+
+        time.sleep(1)
+
+        if h == 22:
+            print("Good night")
+            time.sleep(2)
