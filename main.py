7fb34ee418721dac18c9d2ef80a5cab7f431b617
defensepanda <113334120+defensepanda@users.noreply.github.com>
2022-09-13 17:31:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+from os import system
 
+
+u_name = input("What is your name?\n")
+valid_choise_byhour = {
+    "6":["Shine shoes"],
+    "7":["Shine shoes"],
+    "8":["Shine shoes"],
+    "9":["Shine shoes"]
+}
+
+
+while True:
+    for i in range(6,23,1):
+        choice = ""
+        system("cls")
+        print(f"The clock is now {i}:00")
+        while choice not in valid_choise_byhour[str(i)]:
+            choice = input("What to do\n")
